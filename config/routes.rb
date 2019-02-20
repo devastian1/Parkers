6d18477c7136fadd29bb5881276f00f7023490a8
devastian1 <kevindevastian.com>
2019-02-21 00:24:27 +0800

    addded ajax method for geolocation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  resources :cars do
+  resources :cars
   resources :location
-
-  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	get '/login' => 'sessions#new'
   	post '/login' => 'sessions#create'
   	get '/logout' => 'sessions#destroy'
-    post 'cars/:id/location/new' => 'location#create'
+    post 'location/new' => 'location#create'
   	get '/signup' => 'users#new'
     post '/users' => 'users#create'
     root 'cars#welcome'
